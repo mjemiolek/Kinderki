9310da7797b3f15659c249f28d593f5f53c9d377
Marcin <baryczka.m@gmail.com>
2022-04-05 21:32:36 +0200

    example test

--- Kinderki/main.cpp
+++ Kinderki/main.cpp
@@ -34,8 +34,19 @@ unsigned int loadTexture(char const* path);
 void update(float dt);
 void render();
 void render_gui();
+void RenderText(Shader& shader, std::string text, GLfloat x, GLfloat y, GLfloat scale, glm::vec3 color);
 
 GLFWwindow* window = nullptr;
+
+/// Holds all state information relevant to a character as loaded using FreeType
+struct Character {
+    GLuint TextureID;   // ID handle of the glyph texture
+    glm::ivec2 Size;    // Size of glyph
+    glm::ivec2 Bearing;  // Offset from baseline to left/top of glyph
+    GLuint Advance;    // Horizontal offset to advance to next glyph
+};
+
+std::map<GLchar, Character> Characters;
 
 // settings
 const GLuint SCR_WIDTH = 1280;
@@ -46,6 +57,8 @@ Camera camera(glm::vec3(0.0f, 5.0f, 0.0f));
 float lastX = SCR_WIDTH / 2.0f;
 float lastY = SCR_HEIGHT / 2.0f;
 bool firstMouse = true;
+std::stringstream string_object_name;
+
 
 // timing
 float deltaTime = 0.0f;
@@ -189,6 +202,7 @@ unsigned int skyboxIndices[] =
 };
 
 
+
 GLint u_scale_loc = -1;
 GLint u_world_loc = -1;
 GLint u_scale_loc2 = -1;
@@ -198,6 +212,7 @@ unsigned int VBO = 0;
 //unsigned int quadVAO = 0;
 unsigned int progressVBO1 = 0;
 unsigned int quadVAO, quadVBO1, quadVBO2, quadVBO3, quadEBO;
+GLuint textVAO, textVBO;
 
 struct Transform {
     Transform() : m_world_matrix(1.0f) {}
@@ -352,6 +367,8 @@ int main()
 
     // configure global opengl state
     glEnable(GL_DEPTH_TEST);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
     // build and compile shaders
     // -------------------------
@@ -361,8 +378,8 @@ int main()
     Shader testShader("res/shaders/basic.vert", "res/shaders/basic.frag");
     Shader test2Shader("res/shaders/basic2.vert", "res/shaders/basic2.frag");
     Shader skyboxShader("res/shaders/skybox.vert", "res/shaders/skybox.frag");
+    Shader textShader("res/shaders/text.vert", "res/shaders/text.frag");
     //Shader lightCubeShader("D:/Users/wojci/CLionProjects/OpenGLPAG/res/shaders/lightcube.vert", "D:/Users/wojci/CLionProjects/OpenGLPAG/res/shaders/lightcube.frag");
-
 
     skyboxShader.use();
     glUniform1i(glGetUniformLocation(skyboxShader.ID, "skybox"), 0);
@@ -385,6 +402,17 @@ int main()
 
     // Output {"project":"rapidjson","stars":11}
     std::cout << buffer.GetString() << std::endl;
+
+
+    //FT_Library  library;
+
+    //boolean error = FT_Init_FreeType(&library);
+    //if (error)
+    //{
+    //    std::cout << "an error occurred during library initialization" << std::endl;
+    //}
+
+
     //
 
     //    FT_Library ft;
@@ -603,6 +631,92 @@ int main()
         }
     }
 
+
+    glm::mat4 projection2 = glm::ortho(0.0f, static_cast<GLfloat>(SCR_WIDTH), 0.0f, static_cast<GLfloat>(SCR_HEIGHT));
+    textShader.use();
+    glUniformMatrix4fv(glGetUniformLocation(textShader.ID, "projection"), 1, GL_FALSE, glm::value_ptr(projection2));
+
+    // FreeType
+    FT_Library ft;
+    // All functions return a value different than 0 whenever an error occurred
+    if (FT_Init_FreeType(&ft))
+        std::cout << "ERROR::FREETYPE: Could not init FreeType Library" << std::endl;
+
+    // Load font as face
+    FT_Face face;
+    if (FT_New_Face(ft, "res/fonts/arial.ttf", 0, &face))
+        std::cout << "ERROR::FREETYPE: Failed to load font" << std::endl;
+
+    // Set size to load glyphs as
+    FT_Set_Pixel_Sizes(face, 0, 48);
+
+    // Disable byte-alignment restriction
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+
+    // Load first 128 characters of ASCII set
+    for (GLubyte c = 0; c < 128; c++)
+    {
+        // Load character glyph 
+        if (FT_Load_Char(face, c, FT_LOAD_RENDER))
+        {
+            std::cout << "ERROR::FREETYTPE: Failed to load Glyph" << std::endl;
+            continue;
+        }
+        // Generate texture
+        GLuint texture;
+        glGenTextures(1, &texture);
+        glBindTexture(GL_TEXTURE_2D, texture);
+        glTexImage2D(
+            GL_TEXTURE_2D,
+            0,
+            GL_RED,
+            face->glyph->bitmap.width,
+            face->glyph->bitmap.rows,
+            0,
+            GL_RED,
+            GL_UNSIGNED_BYTE,
+            face->glyph->bitmap.buffer
+        );
+        // Set texture options
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        // Now store character for later use
+        Character character = {
+            texture,
+            glm::ivec2(face->glyph->bitmap.width, face->glyph->bitmap.rows),
+            glm::ivec2(face->glyph->bitmap_left, face->glyph->bitmap_top),
+            face->glyph->advance.x
+        };
+        Characters.insert(std::pair<GLchar, Character>(c, character));
+    }
+    glBindTexture(GL_TEXTURE_2D, 0);
+    // Destroy FreeType once we're finished
+    FT_Done_Face(face);
+    FT_Done_FreeType(ft);
+
+
+    // Configure VAO/VBO for texture quads
+    glGenVertexArrays(1, &textVAO);
+    glGenBuffers(1, &textVBO);
+    glBindVertexArray(textVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, textVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * 6 * 4, NULL, GL_DYNAMIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(GLfloat), 0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+
+
+
+
+
+
+
+
+
     while (!glfwWindowShouldClose(window))
     {
         current_time = glfwGetTime();
@@ -636,7 +750,17 @@ int main()
             glDrawArrays(GL_TRIANGLES, 0, 18);
             glBindVertexArray(0);
 
-            // Since the cubemap will always have a depth of 1.0, we need that equal sign so it doesn't get discarded
+            //
+            //glBindBuffer(GL_ARRAY_BUFFER, textVBO);
+            //glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * 6 * 4, NULL, GL_DYNAMIC_DRAW);
+            RenderText(textShader, "example", 50.0f, 50.0f, 1.0f, glm::vec3(0.5, 0.8f, 0.2f));
+            //string_object_name.clear();
+            //RenderText(textShader, "(C) LearnOpenGL.com", 540.0f, 570.0f, 0.5f, glm::vec3(0.3, 0.7f, 0.9f));
+
+
+
+
+             //Since the cubemap will always have a depth of 1.0, we need that equal sign so it doesn't get discarded
             glDepthFunc(GL_LEQUAL);
 
             skyboxShader.use();
@@ -651,6 +775,7 @@ int main()
 
             // Draws the cubemap as the last object so we can save a bit of performance by discarding all fragments
             // where an object is present (a depth of 1.0f will always fail against any object's depth value)
+
             glBindVertexArray(skyboxVAO);
             glActiveTexture(GL_TEXTURE0);
             glBindTexture(GL_TEXTURE_CUBE_MAP, cubemapTexture);
@@ -789,13 +914,13 @@ void update(float dt) {
         bar[6] = x;
         bar[8] = x;
 
-        std::cout << bar[4] << " " << std::endl;
+        //std::cout << bar[4] << " " << std::endl;
 
     }
     else {
         x = -0.90f;
     }
-
+    string_object_name << current_time;
     root_node->update(Transform(), false);
 }
 void render() {
@@ -807,4 +932,47 @@ void render() {
 void render_gui() {
 
 
+}
+void RenderText(Shader& shader, std::string text, GLfloat x, GLfloat y, GLfloat scale, glm::vec3 color)
+{
+    // Activate corresponding render state	
+    shader.use();
+    glUniform3f(glGetUniformLocation(shader.ID, "textColor"), color.x, color.y, color.z);
+    glActiveTexture(GL_TEXTURE0);
+    glBindVertexArray(textVAO);
+
+    // Iterate through all characters
+    std::string::const_iterator c;
+    for (c = text.begin(); c != text.end(); c++)
+    {
+        Character ch = Characters[*c];
+
+        GLfloat xpos = x + ch.Bearing.x * scale;
+        GLfloat ypos = y - (ch.Size.y - ch.Bearing.y) * scale;
+
+        GLfloat w = ch.Size.x * scale;
+        GLfloat h = ch.Size.y * scale;
+        // Update VBO for each character
+        GLfloat vertices[6][4] = {
+            { xpos,     ypos + h,   0.0, 0.0 },
+            { xpos,     ypos,       0.0, 1.0 },
+            { xpos + w, ypos,       1.0, 1.0 },
+
+            { xpos,     ypos + h,   0.0, 0.0 },
+            { xpos + w, ypos,       1.0, 1.0 },
+            { xpos + w, ypos + h,   1.0, 0.0 }
+        };
+        // Render glyph texture over quad
+        glBindTexture(GL_TEXTURE_2D, ch.TextureID);
+        // Update content of VBO memory
+        glBindBuffer(GL_ARRAY_BUFFER, textVBO);
+        glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices); // Be sure to use glBufferSubData and not glBufferData
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        // Render quad
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        // Now advance cursors for next glyph (note that advance is number of 1/64 pixels)
+        x += (ch.Advance >> 6) * scale; // Bitshift by 6 to get value in pixels (2^6 = 64 (divide amount of 1/64th pixels by 64 to get amount of pixels))
+    }
+    glBindVertexArray(0);
+    glBindTexture(GL_TEXTURE_2D, 0);
 }
